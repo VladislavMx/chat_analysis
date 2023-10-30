aa43e1250bee7a414ccaeaf54e9e437875c6a577
Vladislav331 <Vladyurev111@gmail.com>
2023-10-30 16:48:19 +0300

    изменил запуск, добавил запись в файлы, добавил обработку пропущенных строк

--- main.py
+++ main.py
@@ -1,59 +1,52 @@
-import pandas
+import pandas as pd
 from bertopic import BERTopic
-from translate import Translator
 
 
-global chat_history_file, first_messages_output_file, topics_output_file
-
-chat_history_file = 'Data/chats.csv'
-first_messages_output_file = 'Data/first_messages.csv'
-unique_messages_output_file = 'Data/unique_messages.csv'
-grouped_output_file = 'Data/grouped chat history.csv'
-def parse_chat_history(file_path):
-    try:
-
-        data_frame = pandas.read_csv(file_path,  lineterminator = '\n')
-        print(data_frame)
-        return data_frame
-    except:
-        print("не запарсилось")
-
-###
-def select_first_messages(df):
-
-    blacklist = ['здравствуйте%', 'добрый день%']
-
-    first_messages = df.groupby('session_id').first()
+# Парсинг CSV файла
+def parse_csv_file(file_path):
+    df = pd.read_csv(file_path)
+    return df
 
 
-    for index, row in first_messages.iterrows():#прохожу по файлу получившемуся файлу и склеиваю сообщение
-        if len(row['text']) < 10 or (row['text'] in blacklist):
-            try:
-                next_message = df[(df['session_id'] == index) & (df['role'] == 'user')]['text'].iloc[1]
-                first_messages.loc[index, 'text'] = f"{row['text']} {next_message}"
-            except:
-                print("Вышло за пределы")
+def select_first_user_messages(df):
+    user_messages = []
 
-    first_messages.to_csv(first_messages_output_file, index=False)
+    chat_groups = df.groupby("session_id")
+    for group_key, group_df in chat_groups:
+        first_user_message = group_df.loc[group_df["role"] == "user", "text"].values[0]
+        if len(first_user_message) < 10 or first_user_message.lower().startswith("здравствуйте"):
+            second_user_message = group_df.loc[group_df["role"] == "user", "text"].values[1]
+            first_user_message += " " + second_user_message
 
-def save_unique_messages(data_frame):
+        user_messages.append(first_user_message)
 
-    df = data_frame.drop_duplicates(subset="text")
-
-    df = df["text"].unique()
-
-    df.to_csv(topics_output_file, index=False)
-
-def group_messages_by_topics(df):
-
-    translator = Translator(from_lang="Russian", to_lang="English")
-    model = BERTopic()
-    topics = model.fit_transform(translator.translate(df['message']))
+    return user_messages
 
 
-    df.to_csv(grouped_output_file, index=False)
 
-history_df = parse_chat_history(chat_history_file)
-#select_first_messages(history_df)
-save_unique_messages(history_df)
-group_messages_by_topics(history_df)
+def save_unique_messages(messages, output_file):
+    unique_messages = list(set(messages))
+    pd.DataFrame(unique_messages, columns=["messages"]).to_csv(output_file, index=None)
+
+
+
+def group_messages_by_topics(messages, output_file):
+    topic_model = BERTopic(language="Multilanguage")
+    topics, _ = topic_model.fit_transform(messages)
+    pd.DataFrame({"messages": messages, "topics": topics}).to_csv(output_file, index=None)
+
+
+def main():
+    file_path = "chats.csv"
+    output_file_unique_messages = "unique_messages.csv"
+    output_file_topics = "message_topics.csv"
+
+    df = parse_csv_file(file_path)
+    user_messages = select_first_user_messages(df)
+    save_unique_messages(user_messages, output_file_unique_messages)
+    group_messages_by_topics(user_messages, output_file_topics)
+
+
+if __name__ == "__main__":
+    main()
+
